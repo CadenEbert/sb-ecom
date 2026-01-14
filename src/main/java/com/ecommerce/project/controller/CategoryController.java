22801b97a3947f31d715199495f922aab864d7da
CadenEbert <116670041+CadenEbert@users.noreply.github.com>
2026-01-14 16:27:13 -0600

    bug fix

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,7 +2,6 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +19,7 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @RequestMapping(value="/public/categories", method=RequestMethod.GET)
+    @GetMapping("/public/categories")
     public List<Category> getCategories() {
         return categoryService.getAllCategories();
     }
